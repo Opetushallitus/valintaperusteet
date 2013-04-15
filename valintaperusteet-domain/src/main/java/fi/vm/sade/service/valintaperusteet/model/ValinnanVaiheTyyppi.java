bf9d8d69304609c5d45de3eb5278706c49354330
Kalle Wuoti <kalle.wuoti@gofore.com>
2013-04-15 15:06:27 +0300

    VT-316

---
+++ valintaperusteet-domain/src/main/java/fi/vm/sade/service/valintaperusteet/model/ValinnanVaiheTyyppi.java
@@ -0,0 +1,10 @@
+package fi.vm.sade.service.valintaperusteet.model;
+
+/**
+ * User: kwuoti
+ * Date: 15.4.2013
+ * Time: 14.11
+ */
+public enum ValinnanVaiheTyyppi {
+    TAVALLINEN, VALINTAKOE
+}
