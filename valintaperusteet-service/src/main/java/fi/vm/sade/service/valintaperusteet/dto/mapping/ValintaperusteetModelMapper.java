c1f9030efec8e250ebf82aaea637880f3be4292b
kjsaila <kalle.saila@gofore.com>
2014-01-13 13:12:08 +0200

    pieni paikkaus modelmapperiin, jotta LaskentakaavaService-testit ei hajoa satunnaisesti IDEAlla ajettassa

--- valintaperusteet-service/src/main/java/fi/vm/sade/service/valintaperusteet/dto/mapping/ValintaperusteetModelMapper.java
+++ valintaperusteet-service/src/main/java/fi/vm/sade/service/valintaperusteet/dto/mapping/ValintaperusteetModelMapper.java
@@ -3,11 +3,16 @@ package fi.vm.sade.service.valintaperusteet.dto.mapping;
 
 import fi.vm.sade.service.valintaperusteet.dto.*;
 import fi.vm.sade.service.valintaperusteet.model.*;
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.PropertyMap;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MappingContext;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * User: wuoti
@@ -41,6 +46,46 @@ public class ValintaperusteetModelMapper extends ModelMapper {
             @Override
             protected void configure() {
                 map().setLaskentakaavaId(source.getLaskentakaava().getId());
+            }
+        });
+
+        this.addMappings(new PropertyMap<Funktiokutsu, FunktiokutsuDTO>() {
+            @Override
+            protected void configure() {
+                Converter<Set<Funktioargumentti>, Set<FunktioargumenttiDTO>> funktioargumenttiToDtoConverter = new Converter<Set<Funktioargumentti>, Set<FunktioargumenttiDTO>>() {
+                    public Set<FunktioargumenttiDTO> convert(MappingContext<Set<Funktioargumentti>, Set<FunktioargumenttiDTO>> context) {
+                        Set<FunktioargumenttiDTO> result = new TreeSet<FunktioargumenttiDTO>();
+                        for(Funktioargumentti arg : context.getSource()) {
+                            ModelMapper modelMapper = new ModelMapper();
+                            result.add(modelMapper.map(arg, FunktioargumenttiDTO.class));
+
+                        }
+
+                        return result;
+                    }
+                };
+
+                using(funktioargumenttiToDtoConverter).map(source.getFunktioargumentit()).setFunktioargumentit(null);
+            }
+        });
+
+        this.addMappings(new PropertyMap<FunktiokutsuDTO, Funktiokutsu>() {
+            @Override
+            protected void configure() {
+                Converter<Set<FunktioargumenttiDTO>, Set<Funktioargumentti>> funktioargumenttiToDtoConverter = new Converter<Set<FunktioargumenttiDTO>, Set<Funktioargumentti>>() {
+                    public Set<Funktioargumentti> convert(MappingContext<Set<FunktioargumenttiDTO>, Set<Funktioargumentti>> context) {
+                        Set<Funktioargumentti> result = new TreeSet<Funktioargumentti>();
+
+                        for(FunktioargumenttiDTO arg : context.getSource()) {
+
+                            ModelMapper modelMapper = new ModelMapper();
+                            result.add(modelMapper.map(arg, Funktioargumentti.class));
+                        }
+                        return result;
+                    }
+                };
+
+                using(funktioargumenttiToDtoConverter).map(source.getFunktioargumentit()).setFunktioargumentit(null);
             }
         });
     }
