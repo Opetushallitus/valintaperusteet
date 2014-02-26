934ecffa2c465b7a8434fbbbf93c77365f8344f5
jussijartamo <jussi@jartamo@gofore.com>
2014-02-26 16:14:30 +0200

    valintalaskenta muistissa käyttöliittymä ja mahdollisuus peruutta prosesseja ja esto useamman prosessin yhtäaikaisesta ajosta

--- valintaperusteet-api/src/main/java/fi/vm/sade/service/valintaperusteet/resource/ValintakoeResource.java
+++ valintaperusteet-api/src/main/java/fi/vm/sade/service/valintaperusteet/resource/ValintakoeResource.java
@@ -1,5 +1,7 @@
 package fi.vm.sade.service.valintaperusteet.resource;
 
+import java.util.List;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -18,19 +20,24 @@ import fi.vm.sade.service.valintaperusteet.dto.ValintakoeDTO;
 @Path("valintakoe")
 public interface ValintakoeResource {
 
-    @GET
-    @Path("/{oid}")
-    @Produces(MediaType.APPLICATION_JSON)
-    ValintakoeDTO readByOid(@PathParam("oid") String oid);
+	@GET
+	@Path("/")
+	@Produces(MediaType.APPLICATION_JSON)
+	List<ValintakoeDTO> readAll();
 
-    @POST
-    @Path("/{oid}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    Response update(@PathParam("oid") String oid, ValintakoeDTO valintakoe);
+	@GET
+	@Path("/{oid}")
+	@Produces(MediaType.APPLICATION_JSON)
+	ValintakoeDTO readByOid(@PathParam("oid") String oid);
 
-    @DELETE
-    @Path("/{oid}")
-    Response delete(@PathParam("oid") String oid);
+	@POST
+	@Path("/{oid}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	Response update(@PathParam("oid") String oid, ValintakoeDTO valintakoe);
+
+	@DELETE
+	@Path("/{oid}")
+	Response delete(@PathParam("oid") String oid);
 
 }
