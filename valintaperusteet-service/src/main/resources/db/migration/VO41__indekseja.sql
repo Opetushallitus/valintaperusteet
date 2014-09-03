ebe123c2de17fc56621ad62b290c4f6465eeccdf
kjsaila <kalle.saila@gofore.com>
2014-09-03 14:49:47 +0300

    indeksejä possuun

---
+++ valintaperusteet-service/src/main/resources/db/migration/VO41__indekseja.sql
@@ -0,0 +1,15 @@
+CREATE UNIQUE INDEX funktiokutsu_id_idx ON funktiokutsu (id);
+CREATE UNIQUE INDEX hakijaryhma_jono_oid_idx ON hakijaryhma_jono (oid);
+CREATE UNIQUE INDEX hakukohde_viite_oid_idx ON hakukohde_viite (oid);
+CREATE UNIQUE INDEX jarjestyskriteeri_oid_idx ON jarjestyskriteeri (oid);
+CREATE UNIQUE INDEX laskentakaava_id_idx ON laskentakaava (id);
+CREATE UNIQUE INDEX valinnan_vaihe_oid_idx ON valinnan_vaihe (oid);
+CREATE UNIQUE INDEX valintakoe_oid_idx ON valintakoe (oid);
+CREATE UNIQUE INDEX valintaryhma_oid_idx ON valintaryhma (oid);
+CREATE UNIQUE INDEX valintatapajono_oid_idx ON valintatapajono (oid);
+
+
+
+
+
+
