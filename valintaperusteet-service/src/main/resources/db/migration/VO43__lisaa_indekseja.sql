ac89546e34c64b4f1104f2b61ac1a51ea66ecef7
kjsaila <kalle.saila@gofore.com>
2015-01-22 13:09:01 +0200

    BUG-1 indeksi korjaus

--- valintaperusteet-service/src/main/resources/db/migration/VO43__lisaa_indekseja.sql
+++ valintaperusteet-service/src/main/resources/db/migration/VO43__lisaa_indekseja.sql
@@ -1,4 +1,3 @@
-CREATE UNIQUE INDEX hakukohde_viite_id_idx ON hakukohteen_valintaperuste (hakukohde_viite_id);
 CREATE UNIQUE INDEX hakukohteen_valintaperuste_id_idx ON hakukohteen_valintaperuste (id);
 CREATE UNIQUE INDEX hakijaryhma_oid_idx ON hakijaryhma (oid);
 
