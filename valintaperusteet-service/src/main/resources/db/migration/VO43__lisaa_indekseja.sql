338cf7b54fc1f398e5d09852a57075247f6cae69
kjsaila <kalle.saila@gofore.com>
2015-01-22 12:21:45 +0200

    BUG-1 indeksejä possuun

---
+++ valintaperusteet-service/src/main/resources/db/migration/VO43__lisaa_indekseja.sql
@@ -0,0 +1,11 @@
+CREATE UNIQUE INDEX hakukohde_viite_id_idx ON hakukohteen_valintaperuste (hakukohde_viite_id);
+CREATE UNIQUE INDEX hakukohteen_valintaperuste_id_idx ON hakukohteen_valintaperuste (id);
+CREATE UNIQUE INDEX hakijaryhma_oid_idx ON hakijaryhma (oid);
+
+
+
+
+
+
+
+
