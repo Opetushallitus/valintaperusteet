2c55fc5830dd43e8269b135e0eddfb197ea68fa7
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-06-15 13:47:27 +0300

    remove javadoc and refactor

--- valintaperusteet-service/src/main/java/fi/vm/sade/service/valintaperusteet/roles/ValintaperusteetRole.java
+++ valintaperusteet-service/src/main/java/fi/vm/sade/service/valintaperusteet/roles/ValintaperusteetRole.java
@@ -1,14 +1,6 @@
 package fi.vm.sade.service.valintaperusteet.roles;
 
-/**
- * Created with IntelliJ IDEA.
- * User: jukais
- * Date: 10.6.2013
- * Time: 13.47
- * To change this template use File | Settings | File Templates.
- */
 public class ValintaperusteetRole {
-
     public static final String READ_UPDATE_CRUD = "hasAnyRole('ROLE_APP_VALINTAPERUSTEET_READ','ROLE_APP_VALINTAPERUSTEET_READ_UPDATE','ROLE_APP_VALINTAPERUSTEET_CRUD')";
     public static final String UPDATE_CRUD = "hasAnyRole('ROLE_APP_VALINTAPERUSTEET_READ_UPDATE','ROLE_APP_VALINTAPERUSTEET_CRUD')";
     public static final String CRUD = "hasAnyRole('ROLE_APP_VALINTAPERUSTEET_CRUD')";
