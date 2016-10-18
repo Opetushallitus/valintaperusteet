99e7daf52e50a9c84d05f2d22b86eec4e1a2b8b6
August Kilponen <august.kilponen@gmail.com>
2016-10-18 16:26:55 +0300

    BUG-1186: Korjattu testejä

--- valintaperusteet-service/src/test/java/fi/vm/sade/service/valintaperusteet/dao/ValintakoeDAOTest.java
+++ valintaperusteet-service/src/test/java/fi/vm/sade/service/valintaperusteet/dao/ValintakoeDAOTest.java
@@ -40,7 +40,7 @@ public class ValintakoeDAOTest {
         final String valinnanVaiheOid = "83";
 
         List<Valintakoe> valintakokeet = valintakoeDAO.findByValinnanVaihe(valinnanVaiheOid);
-        assertEquals(4, valintakokeet.size());
+        assertEquals(5, valintakokeet.size());
 
         Collections.sort(valintakokeet, new Comparator<Valintakoe>() {
             @Override
@@ -51,9 +51,10 @@ public class ValintakoeDAOTest {
 
 
         assertEquals("oid1", valintakokeet.get(0).getOid());
-        assertEquals("oid2", valintakokeet.get(1).getOid());
-        assertEquals("oid3", valintakokeet.get(2).getOid());
-        assertEquals("oid4", valintakokeet.get(3).getOid());
+        assertEquals("oid17", valintakokeet.get(1).getOid());
+        assertEquals("oid2", valintakokeet.get(2).getOid());
+        assertEquals("oid3", valintakokeet.get(3).getOid());
+        assertEquals("oid4", valintakokeet.get(4).getOid());
 
     }
 }
