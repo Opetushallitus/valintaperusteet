d99bf247dd8428b7d0246e7f480307a6ea783799
Esko Suomi <esko.suomi@siili.com>
2017-02-09 09:59:34 +0200

    Add explicit priority to HakijaryhmaDTO for transferring the ordering info reliably over all APIs

--- valintaperusteet-api/src/main/java/fi/vm/sade/service/valintaperusteet/dto/HakijaryhmaDTO.java
+++ valintaperusteet-api/src/main/java/fi/vm/sade/service/valintaperusteet/dto/HakijaryhmaDTO.java
@@ -3,13 +3,13 @@ package fi.vm.sade.service.valintaperusteet.dto;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @ApiModel(value = "HakijaryhmaDTO", description = "Hakijaryhmä")
-public class HakijaryhmaDTO extends HakijaryhmaCreateDTO {
+public class HakijaryhmaDTO extends HakijaryhmaCreateDTO implements Prioritized {
     @ApiModelProperty(value = "OID", required = true)
     private String oid;
+
+    @ApiModelProperty(value = "Hakijaryhmän prioriteetti")
+    private int prioriteetti;
 
     public String getOid() {
         return oid;
@@ -19,4 +19,13 @@ public class HakijaryhmaDTO extends HakijaryhmaCreateDTO {
         this.oid = oid;
     }
 
+    @Override
+    public int getPrioriteetti() {
+        return prioriteetti;
+    }
+
+    @Override
+    public void setPrioriteetti(int prioriteetti) {
+        this.prioriteetti = prioriteetti;
+    }
 }
