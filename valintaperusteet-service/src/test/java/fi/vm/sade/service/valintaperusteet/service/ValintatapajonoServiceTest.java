2bf8e6f6aae1df7ccb1f37f31e25bcefbc16b2d4
Joni Mäkelä <joni.makela@gmail.com>
2017-09-13 12:38:42 +0300

    Removed commented out code

--- valintaperusteet-service/src/test/java/fi/vm/sade/service/valintaperusteet/service/ValintatapajonoServiceTest.java
+++ valintaperusteet-service/src/test/java/fi/vm/sade/service/valintaperusteet/service/ValintatapajonoServiceTest.java
@@ -534,24 +534,3 @@ class VtsRestClientConfig {
         return mock;
     }
 }
-/*
-final class WithUserDetailsSecurityContextFactory
-        implements WithSecurityContextFactory<WithUserDetails> {
-
-    private UserDetailsService userDetailsService;
-
-    @Autowired
-    public WithUserDetailsSecurityContextFactory(UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
-    }
-
-    public SecurityContext createSecurityContext(WithUserDetails withUser) {
-        String username = withUser.value();
-        Assert.hasLength(username, "value() must be non-empty String");
-        UserDetails principal = userDetailsService.loadUserByUsername(username);
-        Authentication authentication = new UsernamePasswordAuthenticationToken(principal, principal.getPassword(), principal.getAuthorities());
-        SecurityContext context = SecurityContextHolder.createEmptyContext();
-        context.setAuthentication(authentication);
-        return context;
-    }
-}*/
