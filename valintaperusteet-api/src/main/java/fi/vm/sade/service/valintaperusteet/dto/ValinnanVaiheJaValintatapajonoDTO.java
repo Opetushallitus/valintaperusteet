d7906b6ba4fd13bf449d30461fc046061325b02f
Jessika Penttinen <jessika.penttinen@siili.com>
2017-11-22 14:20:57 +0200

    Palautetaan valintatapajonojen ohella tieto siitä, kuuluuko valinnanvaihe sijoitteluun

--- valintaperusteet-api/src/main/java/fi/vm/sade/service/valintaperusteet/dto/ValinnanVaiheJaValintatapajonoDTO.java
+++ valintaperusteet-api/src/main/java/fi/vm/sade/service/valintaperusteet/dto/ValinnanVaiheJaValintatapajonoDTO.java
@@ -5,15 +5,21 @@ import java.util.List;
 public class ValinnanVaiheJaValintatapajonoDTO {
 
     private String valinnanvaiheOid;
+    private Boolean kuuluuSijoitteluun;
     private List<ValintatapajonoDTO> valintatapajonot;
 
-    public ValinnanVaiheJaValintatapajonoDTO(String valinnanvaiheOid, List<ValintatapajonoDTO> valintatapajonot) {
+    public ValinnanVaiheJaValintatapajonoDTO(String valinnanvaiheOid, Boolean kuuluuSijoitteluun, List<ValintatapajonoDTO> valintatapajonot) {
         this.valinnanvaiheOid = valinnanvaiheOid;
+        this.kuuluuSijoitteluun = kuuluuSijoitteluun;
         this.valintatapajonot = valintatapajonot;
     }
 
     public String getValinnanvaiheOid() {
         return valinnanvaiheOid;
+    }
+
+    public Boolean getKuuluuSijoitteluun() {
+        return kuuluuSijoitteluun;
     }
 
     public List<ValintatapajonoDTO> getValintatapajonot() {
