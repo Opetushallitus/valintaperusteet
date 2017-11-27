3d96338eed27f7907833cec39a95a53884220304
Jessika Penttinen <jessika.penttinen@siili.com>
2017-11-27 11:03:35 +0200

    Sekä valintatapajonoihin että hakukohteeseen linketettyjen hakijaryhmien hakeminen

--- valintaperusteet-service/src/main/java/fi/vm/sade/service/valintaperusteet/service/HakijaryhmaValintatapajonoService.java
+++ valintaperusteet-service/src/main/java/fi/vm/sade/service/valintaperusteet/service/HakijaryhmaValintatapajonoService.java
@@ -21,8 +21,6 @@ public interface HakijaryhmaValintatapajonoService {
 
     List<HakijaryhmaValintatapajono> findByHakukohteet(Collection<String> hakukohdeOids);
 
-    List<HakijaryhmaValintatapajono> findByHakukohteetWithValintatapajono(Collection<String> hakukohdeOids);
-
     HakijaryhmaValintatapajono update(String oid, HakijaryhmaValintatapajonoDTO dto);
 
     void liitaHakijaryhmaValintatapajonolle(String valintatapajonoOid, String hakijaryhmaOid);
