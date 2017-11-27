918f0131edc4c1c58f1a60064ce8d54bc19be512
Jessika Penttinen <jessika.penttinen@siili.com>
2017-11-27 11:03:35 +0200

    Sekä valintatapajonoihin että hakukohteeseen linketettyjen hakijaryhmien hakeminen

---
+++ valintaperusteet-api/src/main/java/fi/vm/sade/service/valintaperusteet/dto/HakukohdeJaLinkitettyHakijaryhmaValintatapajonoDTO.java
@@ -0,0 +1,22 @@
+package fi.vm.sade.service.valintaperusteet.dto;
+
+import java.util.List;
+
+public class HakukohdeJaLinkitettyHakijaryhmaValintatapajonoDTO {
+    private String hakukohdeOid;
+    private List<LinkitettyHakijaryhmaValintatapajonoDTO> hakijaryhmat;
+
+    public HakukohdeJaLinkitettyHakijaryhmaValintatapajonoDTO(String hakukohdeOid,
+                                                              List<LinkitettyHakijaryhmaValintatapajonoDTO> hakijaryhmat) {
+        this.hakukohdeOid = hakukohdeOid;
+        this.hakijaryhmat = hakijaryhmat;
+    }
+
+    public String getHakukohdeOid() {
+        return hakukohdeOid;
+    }
+
+    public List<LinkitettyHakijaryhmaValintatapajonoDTO> getHakijaryhmat() {
+        return hakijaryhmat;
+    }
+}
