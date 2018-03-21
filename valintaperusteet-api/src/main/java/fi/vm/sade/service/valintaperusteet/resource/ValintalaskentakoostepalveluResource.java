623554f0a01874b9bfb10bec33e7be9a3ffcf320
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2018-03-21 14:46:09 +0200

    OY-177 : Clarifying comment

--- valintaperusteet-api/src/main/java/fi/vm/sade/service/valintaperusteet/resource/ValintalaskentakoostepalveluResource.java
+++ valintaperusteet-api/src/main/java/fi/vm/sade/service/valintaperusteet/resource/ValintalaskentakoostepalveluResource.java
@@ -14,6 +14,9 @@ import java.util.Map;
 @Path("/valintalaskentakoostepalvelu")
 public interface ValintalaskentakoostepalveluResource {
 
+    /**
+     * @return sijoitteluun siirrettävät valintatapajonot hakukohdeoideittain
+     */
     @POST
     @Path("/valintatapajono")
     @Produces(MediaType.APPLICATION_JSON)
