2139e00dd15e7973459f488544fb978519e15ec9
oph-petar <petar@phz.fi>
2018-10-23 15:20:54 +0300

    Explicit include of javax.ws stuff

--- valintaperusteet-service/src/main/java/fi/vm/sade/service/valintaperusteet/resource/impl/HakukohdeResourceImpl.java
+++ valintaperusteet-service/src/main/java/fi/vm/sade/service/valintaperusteet/resource/impl/HakukohdeResourceImpl.java
@@ -8,7 +8,17 @@ import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
