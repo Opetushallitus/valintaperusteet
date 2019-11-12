5b445d09a712045cf6f77407689e91772a7fe9bf
Harri Salokorpi <harri.salokorpi@reaktor.com>
2019-11-12 17:15:44 +0200

    VTKU-98: Implement score extraction and pipeline for returning only the score data

--- valintaperusteet-laskenta/src/test/scala/fi/vm/sade/service/valintaperusteet/laskenta/LensTest.scala
+++ valintaperusteet-laskenta/src/test/scala/fi/vm/sade/service/valintaperusteet/laskenta/LensTest.scala
@@ -15,27 +15,62 @@ object LensTest {
 
   def main(args: Array[String]): Unit = {
     val json = loadJson("koski-opiskeluoikeudet.json")
-    val cutoffDate = DateTime.now()
+    val sulkeutumisPäivämäärä = DateTime.now()
     val suorituksenSallitutKoodit = Set(1, 4, 26)
+    val osasuorituksenSallitutKoodit = Set("101054")
 
-    onkoHaluttujaTutkintoja(json, cutoffDate, "ammatillinenkoulutus", suorituksenSallitutKoodit)
+
+    run(json, sulkeutumisPäivämäärä, "ammatillinenkoulutus", suorituksenSallitutKoodit, osasuorituksenSallitutKoodit)
   }
 
-  private def onkoHaluttujaTutkintoja(json: Json, cutoffDate: DateTime, opiskeluoikeudenHaluttuTyyppi: String, suorituksenSallitutKoodit: Set[Int]): Unit = {
-    val dateFormat = DateTimeFormat.forPattern("yyyy-MM-dd")
+  private def run(json: Json, sulkeutumisPäivämäärä: DateTime, opiskeluoikeudenHaluttuTyyppi: String, suorituksenSallitutKoodit: Set[Int], osasuorituksenSallitutKoodit: Set[String]): Unit = {
+    val valmiitTutkinnot: Seq[Json] = etsiValmiitTutkinnot(json, opiskeluoikeudenHaluttuTyyppi)
 
-    // Opiskeluoikeudet etsivä linssi
-    val _opiskeluoikeudet = JsonPath.root.each.json
+    val yhteistenTutkinnonOsienArvosanat = valmiitTutkinnot
+      .flatMap(tutkinto => etsiValiditSuoritukset(tutkinto, sulkeutumisPäivämäärä, suorituksenSallitutKoodit))
+      .map(suoritus => etsiYhteisetTutkinnonOsat(suoritus, sulkeutumisPäivämäärä, osasuorituksenSallitutKoodit))
+
+    yhteistenTutkinnonOsienArvosanat.foreach(arvosana => println("Arvosana: %s".format(arvosana)))
+  }
+
+  private def etsiYhteisetTutkinnonOsat(suoritus: Json, sulkeutumisPäivämäärä: DateTime, osasuorituksenSallitutKoodit: Set[String]): List[(String, String, Int)] = {
+    // Suoritusten alla olevien osasuoritusten tietoja etsivä linssi
+    val _osasuoritukset = JsonPath.root.osasuoritukset.each.json
+
+    // Osasuorituksen rakennetta purkavat linssit
+    val _osasuorituksenKoulutusmoduuli = JsonPath.root.koulutusmoduuli
+    val _osasuorituksenKoodiarvo = _osasuorituksenKoulutusmoduuli.tunniste.koodiarvo.string
+    val _osasuorituksenNimiFi = _osasuorituksenKoulutusmoduuli.tunniste.nimi.fi.string
+    val _osasuorituksenArviointi = JsonPath.root.arviointi.each.json
+
+    _osasuoritukset.getAll(suoritus).filter(osasuoritus => {
+      osasuorituksenSallitutKoodit.contains(_osasuorituksenKoodiarvo.getOption(osasuoritus).orNull)
+    }).map(osasuoritus => {
+      val osasuorituksenKoodiarvo = _osasuorituksenKoodiarvo.getOption(osasuoritus).orNull
+      val osasuorituksenNimiFi = _osasuorituksenNimiFi.getOption(osasuoritus).orNull
+      val (_, osasuorituksenUusinHyväksyttyArvio): (String, String) = _osasuorituksenArviointi.getAll(osasuoritus)
+        .filter(arvio => JsonPath.root.hyväksytty.boolean.getOption(arvio).getOrElse(false))
+        .map(arvio => (
+          JsonPath.root.päivä.string.getOption(arvio).orNull,
+          JsonPath.root.arvosana.koodiarvo.string.getOption(arvio).orNull
+        )).sorted(Ordering.Tuple2(Ordering.String.reverse, Ordering.String))
+          .head
+
+      println("Osasuorituksen arvio: %s".format(osasuorituksenUusinHyväksyttyArvio))
+      println("Osasuorituksen nimi: %s".format(osasuorituksenNimiFi))
+      println("Osasuorituksen koodiarvo: %s".format(osasuorituksenKoodiarvo))
+
+      (osasuorituksenKoodiarvo, osasuorituksenNimiFi, osasuorituksenUusinHyväksyttyArvio.toInt)
+    })
+  }
+
+  private def etsiValiditSuoritukset(tutkinto: Json, sulkeutumisPäivämäärä: DateTime, suorituksenSallitutKoodit: Set[Int]): List[Json] = {
+    val dateFormat = DateTimeFormat.forPattern("yyyy-MM-dd")
 
     // Suoritukset etsivä linssi
     val _suoritukset = JsonPath.root.suoritukset.each.json
 
-    // Suoritusten alla olevien osasuoritusten tietoja etsivä linssi
-    val _osasuoritukset = JsonPath.root.osasuoritukset.each.json
-
-    // Opiskeluoikeuden tyypin etsivä linssi
-    val _opiskeluoikeudenTyyppi = JsonPath.root.tyyppi.koodiarvo.string
-    val _valmistumisTila = JsonPath.root.tila.opiskeluoikeusjaksot.each.tila.koodiarvo.string
+    val _vahvistusPvm = JsonPath.root.vahvistus.päivä.string
 
     // Suorituksen rakennetta purkavat linssit
     val _koulutusmoduuli = JsonPath.root.koulutusmoduuli
@@ -44,14 +79,41 @@ object LensTest {
     val _koulutusTyypinKoodiarvo = _koulutusmoduuli.koulutustyyppi.koodiarvo.string
     val _koulutusTyypinNimiFi = _koulutusmoduuli.koulutustyyppi.nimi.fi.string
 
-    val _vahvistusPvm = JsonPath.root.vahvistus.päivä.string
+    _suoritukset.getAll(tutkinto).filter(suoritus => {
+      val koodiarvo = _koodiarvo.getOption(suoritus).orNull
+      val lyhytNimiFi = _lyhytNimiFi.getOption(suoritus).orNull
+      val koulutusTyypinNimiFi = _koulutusTyypinNimiFi.getOption(suoritus).orNull
 
-    // Osasuorituksen rakennetta purkavat linssit
-    val _osasuorituksenKoulutusmoduuli = JsonPath.root.koulutusmoduuli
-    val _osasuorituksenKoodiarvo = _osasuorituksenKoulutusmoduuli.tunniste.koodiarvo.string
-    val _osasuorituksenNimiFi = _osasuorituksenKoulutusmoduuli.tunniste.nimi.fi.string
+      val vahvistettuRajapäivänä = _vahvistusPvm.getOption(suoritus) match {
+        case Some(dateString) => sulkeutumisPäivämäärä.isAfter(dateFormat.parseDateTime(dateString))
+        case None => false
+      }
+      val koulutusTyypinKoodiarvo = _koulutusTyypinKoodiarvo.getOption(suoritus) match {
+        case Some(s) => s.toInt
+        case None => -1
+      }
+      val onkoSallitunTyyppinenSuoritus = suorituksenSallitutKoodit.contains(koulutusTyypinKoodiarvo)
 
-    _opiskeluoikeudet.getAll(json).foreach(opiskeluoikeus => {
+      println("Koodiarvo: %s".format(koodiarvo))
+      println("Onko sallitun tyyppinen suoritus: %s".format(onkoSallitunTyyppinenSuoritus))
+      println("Nimi: %s".format(lyhytNimiFi))
+      println("Koulutustyypin nimi: %s".format(koulutusTyypinNimiFi))
+      println("Koulutustyypin koodiarvo: %s".format(koulutusTyypinKoodiarvo))
+      println("On vahvistettu rajapäivänä pvm: %s".format(vahvistettuRajapäivänä))
+
+      onkoSallitunTyyppinenSuoritus && vahvistettuRajapäivänä
+    })
+  }
+
+  private def etsiValmiitTutkinnot(json: Json, opiskeluoikeudenHaluttuTyyppi: String): Seq[Json] = {
+    // Opiskeluoikeudet etsivä linssi
+    val _opiskeluoikeudet = JsonPath.root.each.json
+
+    // Opiskeluoikeuden tyypin etsivä linssi
+    val _opiskeluoikeudenTyyppi = JsonPath.root.tyyppi.koodiarvo.string
+    val _valmistumisTila = JsonPath.root.tila.opiskeluoikeusjaksot.each.tila.koodiarvo.string
+
+    _opiskeluoikeudet.getAll(json).filter(opiskeluoikeus => {
       val opiskeluoikeudenTyyppi = _opiskeluoikeudenTyyppi.getOption(opiskeluoikeus).orNull
       val valmistumisTila = _valmistumisTila.getAll(opiskeluoikeus)
 
@@ -63,38 +125,7 @@ object LensTest {
       println("Onko valmistunut: %s".format(onkoValmistunut))
       println("Onko ammatillinen opiskeluoikeus: %s".format(onkoAmmatillinenOpiskeluOikeus))
 
-      _suoritukset.getAll(opiskeluoikeus).foreach(suoritus => {
-        val koodiarvo = _koodiarvo.getOption(suoritus).orNull
-        val lyhytNimiFi = _lyhytNimiFi.getOption(suoritus).orNull
-        val koulutusTyypinNimiFi = _koulutusTyypinNimiFi.getOption(suoritus).orNull
-        val vahvistettuRajapäivänä = _vahvistusPvm.getOption(suoritus) match {
-          case Some(dateString) => cutoffDate.isAfter(dateFormat.parseDateTime(dateString))
-          case None => false
-        }
-
-        val koulutusTyypinKoodiarvo = _koulutusTyypinKoodiarvo.getOption(suoritus) match {
-          case Some(s) => s.toInt
-          case None => -1
-        }
-        val onkoSallitunTyyppinenSuoritus = suorituksenSallitutKoodit.contains(koulutusTyypinKoodiarvo)
-
-        println("Koodiarvo: %s".format(koodiarvo))
-        println("Onko sallitun tyyppinen suoritus: %s".format(onkoSallitunTyyppinenSuoritus))
-        println("Nimi: %s".format(lyhytNimiFi))
-        println("Koulutustyypin nimi: %s".format(koulutusTyypinNimiFi))
-        println("Koulutustyypin koodiarvo: %s".format(koulutusTyypinKoodiarvo))
-        println("On vahvistettu rajapäivänä pvm: %s".format(vahvistettuRajapäivänä))
-
-        println("Osasuoritukset")
-        _osasuoritukset.getAll(suoritus).foreach(osasuoritus => {
-          val osasuorituksenNimiFi = _osasuorituksenNimiFi.getOption(osasuoritus).orNull
-          val osasuorituksenKoodiarvo = _osasuorituksenKoodiarvo.getOption(osasuoritus).orNull
-
-          println("Osasuorituksen nimi: %s".format(osasuorituksenNimiFi))
-          println("Osasuorituksen koodiarvo: %s".format(osasuorituksenKoodiarvo))
-        })
-      })
-
+      onkoAmmatillinenOpiskeluOikeus
     })
   }
 }
