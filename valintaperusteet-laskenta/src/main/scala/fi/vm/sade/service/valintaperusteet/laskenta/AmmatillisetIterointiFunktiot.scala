46fd1b9fa7082d2b3bae4093d5c5d57d2c7a35f3
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2020-02-12 15:31:39 +0200

    VTKU-115 : Korjaa funktion nimi

--- valintaperusteet-laskenta/src/main/scala/fi/vm/sade/service/valintaperusteet/laskenta/AmmatillisetIterointiFunktiot.scala
+++ valintaperusteet-laskenta/src/main/scala/fi/vm/sade/service/valintaperusteet/laskenta/AmmatillisetIterointiFunktiot.scala
@@ -191,7 +191,7 @@ trait AmmatillisetIterointiFunktiot {
           laskeLukuarvo(iteroidutTuloksetKasittelevaKlooni, Map())
         } catch {
           case e: ClassCastException => {
-            Laskin.LOG.error(s"${classOf[IteroiAmmatillisetTutkinnot].getSimpleName} -funktion funktioargumenttina tulee olla " +
+            Laskin.LOG.error(s"${classOf[IteroiAmmatillisenTutkinnonYtoOsaAlueet].getSimpleName} -funktion funktioargumenttina tulee olla " +
               s"kloonattava funktio, kuten maksimi, mutta oli $lapsiFunktio", e)
             throw e
           }
