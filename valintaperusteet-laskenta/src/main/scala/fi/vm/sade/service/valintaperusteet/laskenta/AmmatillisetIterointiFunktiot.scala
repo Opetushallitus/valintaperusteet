da5f771751982f815b4a9c16834572dd8539cdc4
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2020-02-19 14:23:46 +0200

    VTKU-115 : Korjaa teksti

--- valintaperusteet-laskenta/src/main/scala/fi/vm/sade/service/valintaperusteet/laskenta/AmmatillisetIterointiFunktiot.scala
+++ valintaperusteet-laskenta/src/main/scala/fi/vm/sade/service/valintaperusteet/laskenta/AmmatillisetIterointiFunktiot.scala
@@ -202,7 +202,7 @@ trait AmmatillisetIterointiFunktiot {
       val tilalista = List(tulos.tila)
       val avaimet = Map(
         s"ammatillisen perustutkinnon ${tutkinnonValitsija.lyhytKuvaus} YTOn $ytoKoodi osa-alueiden määrä" -> Some(ytoOsaAlueet.size),
-        s"ammatillisen perustutkinnon ${tutkinnonValitsija.lyhytKuvaus} YTOn $ytoKoodi" -> Some(uudetParametrit.map(_.kuvaus).mkString("; ")))
+        s"ammatillisen perustutkinnon ${tutkinnonValitsija.lyhytKuvaus} YTOn $ytoKoodi osa-alueet" -> Some(uudetParametrit.map(_.kuvaus).mkString("; ")))
       (tulos.tulos, tilalista, Historia(ITEROIAMMATILLISETYTOOSAALUEET, tulos.tulos, tilalista, Some(kierrostenHistoriatKahdelleParametrille(kierrostenTulokset)), Some(avaimet)))
     }
   }
