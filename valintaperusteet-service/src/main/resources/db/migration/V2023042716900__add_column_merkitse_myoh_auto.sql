9d2f9de8239af962c61328230de4632340258ef3
Risto Salama <risto.salama@gofore.com>
2023-05-03 17:30:32 +0300

    Fix migration

--- valintaperusteet-service/src/main/resources/db/migration/V2023042716900__add_column_merkitse_myoh_auto.sql
+++ valintaperusteet-service/src/main/resources/db/migration/V2023042716900__add_column_merkitse_myoh_auto.sql
@@ -1,3 +1,3 @@
-alter table valintatapajono add column merkitse_myoh_auto boolean;
+alter table valintatapajono add column merkitse_myoh_auto boolean default false;
 update valintatapajono set merkitse_myoh_auto = false;
 alter table valintatapajono alter column merkitse_myoh_auto set not null;
