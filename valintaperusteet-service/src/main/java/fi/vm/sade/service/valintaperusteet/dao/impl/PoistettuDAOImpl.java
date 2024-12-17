9787b8e275d77b26ccc7514179854ec331a1d657
August Kilponen <august.kilponen@gmail.com>
2024-12-17 14:41:38 +0200

    Ajettu Spotless

--- valintaperusteet-service/src/main/java/fi/vm/sade/service/valintaperusteet/dao/impl/PoistettuDAOImpl.java
+++ valintaperusteet-service/src/main/java/fi/vm/sade/service/valintaperusteet/dao/impl/PoistettuDAOImpl.java
@@ -10,7 +10,6 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.stereotype.Repository;
 
 @Repository
