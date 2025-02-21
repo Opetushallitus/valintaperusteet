945044796e27854bebad71129ac4d546f4004885
Santeri Korri <santeri.korri@knowit.fi>
2025-02-21 17:32:50 +0200

    OK-798 Poistettu orpojen funktiokutsujen poistotoiminnallisuus

--- valintaperusteet-service/src/main/java/fi/vm/sade/service/valintaperusteet/service/impl/actors/ActorService.java
+++ valintaperusteet-service/src/main/java/fi/vm/sade/service/valintaperusteet/service/impl/actors/ActorService.java
@@ -21,19 +21,10 @@ public class ActorService {
 
   private ActorSystem actorSystem;
 
-  private ActorRef deleteOrphans;
-
-  private Cancellable cancellable;
-
-  private FiniteDuration duration;
-
   @PostConstruct
   private void initActorSystemAndSchedulers() {
     actorSystem = ActorSystem.create("ValintaperusteetActorSystem");
     SpringExtProvider.get(actorSystem).initialize(applicationContext);
-    initOrphanRemovalActor();
-    duration = Duration.create(6, TimeUnit.HOURS);
-    runScheduler(duration, deleteOrphans);
   }
 
   @PreDestroy
@@ -43,44 +34,5 @@ public class ActorService {
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
-  }
-
-  private void initOrphanRemovalActor() {
-    deleteOrphans =
-        actorSystem.actorOf(
-            SpringExtProvider.get(actorSystem).props("PoistaOrvotActorBean"), "OrphanRemovalActor");
-  }
-
-  private void runScheduler(FiniteDuration duration, ActorRef actor) {
-    cancellable =
-        actorSystem
-            .scheduler()
-            .schedule(
-                Duration.Zero(),
-                duration,
-                actor,
-                "run",
-                actorSystem.dispatcher(),
-                ActorRef.noSender());
-  }
-
-  public ActorSystem getActorSystem() {
-    return actorSystem;
-  }
-
-  public void runSchedulerIfNotRunning() {
-    if (deleteOrphans.isTerminated()) {
-      if (!cancellable.isCancelled()) {
-        cancellable.cancel();
-      }
-      initOrphanRemovalActor();
-      runScheduler(duration, deleteOrphans);
-    } else if (cancellable.isCancelled()) {
-      runScheduler(duration, deleteOrphans);
-    }
-  }
-
-  public void runOnce() {
-    deleteOrphans.tell("run", ActorRef.noSender());
   }
 }
