b120c3d9c8ce0cb3faa4699875ed0c71a07daff7
Santeri Korri <santeri.korri@knowit.fi>
2025-02-21 17:37:23 +0200

    OK-798 Poistettu funktiokutsujen migraatiotoiminnallisuus

--- valintaperusteet-service/src/main/java/fi/vm/sade/service/valintaperusteet/App.java
+++ valintaperusteet-service/src/main/java/fi/vm/sade/service/valintaperusteet/App.java
@@ -2,10 +2,8 @@ package fi.vm.sade.service.valintaperusteet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class App {
   public static final String CONTEXT_PATH = "/valintaperusteet-service";
 
