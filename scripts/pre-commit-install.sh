e489697ee52a60bdc249d87945fa3c8c4ebea40f
Petteri Tolonen <petteri.tolonen@knowit.fi>
2025-03-17 11:16:46 +0200

    Lisätty dokumentaatiota spotlessille ja pre-commit hookille

--- scripts/pre-commit-install.sh
+++ scripts/pre-commit-install.sh
@@ -10,7 +10,7 @@ output=$(pre-commit install -t pre-commit 2>&1)
 if [[ $? -ne 0 ]]
 then
   echo $output
-  echo "Failed to install pre-commit hook. Please install pre-commit by running 'pip install pre-commit'. More info at https://pre-commit.com/"
+  echo "Failed to install Git pre-commit hook. Please install pre-commit by running 'pip install pre-commit'. More info at https://pre-commit.com/"
   exit 1
 else
   echo $output
