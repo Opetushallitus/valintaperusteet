f2955aba0390fc6cdf16fc92dc2d1d45276cf8c0
Petteri Tolonen <petteri.tolonen@knowit.fi>
2025-06-03 15:19:42 +0300

    OK-986: Nostettu versionumeroita ja lisätty swagger-annotaatio HakukohdeKoosteTietoDTO:lle

--- valintaperusteet-api/src/main/java/fi/vm/sade/service/valintaperusteet/dto/HakukohdeKoosteTietoDTO.java
+++ valintaperusteet-api/src/main/java/fi/vm/sade/service/valintaperusteet/dto/HakukohdeKoosteTietoDTO.java
@@ -1,7 +1,11 @@
 package fi.vm.sade.service.valintaperusteet.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.Date;
 
+@Schema(
+    name = "HakukohdeKoosteTietoDTO",
+    description = "Hakukohteen valintaperusteista koostettuja tietoja")
 public class HakukohdeKoosteTietoDTO {
   public final String hakukohdeOid;
   public final Boolean hasValintakoe;
